05e3f95ae3fc15485b4afed4eb8e527f6c55b7e8
ifpb <oliveira.julio@academico.ifpb.edu.br>
2024-10-30 13:35:53 -0300

    init spring project

---
+++ src/main/java/com/server/user/domain/User.java
@@ -0,0 +1,21 @@
+package com.server.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User  {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
